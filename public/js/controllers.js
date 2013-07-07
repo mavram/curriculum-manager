05bdff0d892e39b4ac1a79ddcf79a0b691fddf49
mavram <mavram@gmail.com>
2013-07-07 12:33:56 -0400

    REST services for Account Settings

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,10 +6,9 @@
 angular.module('K12.controllers', [])
     .controller('AppCtrl', ['$scope', function ($scope) {
     }])
-    .controller('AccountCtrl', ['$scope', '$http', function ($scope, $http) {
-        $http.get('/api/v.1/account/:').success(function(data) {
-            console.log(data);
-            $scope.username = 'hekademos';
+    .controller('UserCtrl', ['$scope', '$http', function ($scope, $http) {
+        $http.get('/api/v.1/user/accountSettings').success(function(data) {
+            $scope.username = data.username;
         });
 
     }]);
