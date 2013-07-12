ff126dac1f1187021b4051ebf07273db62a3fa4f
mavram <mavram@gmail.com>
2013-07-12 18:57:01 -0400

    Added to repository

---
+++ public/js/services.js
@@ -0,0 +1,32 @@
+/*
+ * Services
+ */
+
+'use strict';
+
+angular.module('K12.services', []).factory('AuthSvc', function($http, $cookieStore) {
+    var currentUser = $cookieStore.get('_k12_user');
+    $cookieStore.remove('_k12_user');
+
+    return {
+        signin: function(user, success, error) {
+            $http.post('/api/v.1/auth/signin', user).success(function(user){
+                currentUser = user;
+                success();
+            }).error(function(msg) {
+                error(msg);
+            });
+        },
+
+        signout: function(success, error) {
+            $http.get('/api/v.1/auth/signout').success(function(){
+                currentUser = {};
+                success();
+            }).error(error);
+        },
+
+        user: function () {
+            return currentUser;
+        },
+    };
+});
