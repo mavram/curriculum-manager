2b8a4693b9577e7d1a2f749e3d32516ef2bde985
Mircea Avram <mavram@gmail.com>
2013-07-12 20:11:01 -0400

    Added UserSvc

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,9 +6,10 @@
 
 angular.module('K12.controllers', [])
     .controller('AppCtrl',['$rootScope', '$scope', '$location', 'AuthSvc', function($rootScope, $scope, $location, AuthSvc) {
-        $scope.user = AuthSvc.user();
+        console.log('AppCtrl:init: .......');
+        console.log(JSON.stringify($scope));
 
-        //console.log('AppCtrl:init:user:' + JSON.stringify($scope.user));
+        $scope.user = AuthSvc.user();
 
         $scope.signout = function() {
             AuthSvc.signout(function() {
@@ -20,6 +21,9 @@ angular.module('K12.controllers', [])
     }])
 
     .controller('AuthCtrl',['$scope', '$location', 'AuthSvc', '$cookieStore', function($scope, $location, AuthSvc) {
+        console.log('AuthCtrl:init: .......');
+        console.log(JSON.stringify($scope));
+
         $scope.rememberMe = AuthSvc.doesRememberMe();
         $scope.signin = function() {
             AuthSvc.signin({
@@ -34,7 +38,23 @@ angular.module('K12.controllers', [])
         };
     }])
 
+    .controller('UserCtrl', ['$scope', '$http', 'UserSvc', function ($scope, $http, $UserSvc) {
+        console.log('UserCtrl:init: .......');
+        console.log(JSON.stringify($scope));
+
+        $scope.settings = function() {
+            UserSvc.settings(function(settings) {
+                $scope.settings = settings;
+            }, function(msg) {
+                $scope.error = msg;
+            });
+        };
+    }])
+
     .controller('HierarchyCtrl', ['$scope', '$http', function ($scope, $http) {
+        console.log('HierarchyCtrl:init: .......');
+        console.log(JSON.stringify($scope));
+
         $http.get('/api/v.1/hierarchy/curricula').success(function(data) {
             $scope.curricula = data;
         });
@@ -46,11 +66,5 @@ angular.module('K12.controllers', [])
         });
         $http.get('/api/v.1/hierarchy/skills').success(function(data) {
             $scope.skills = data;
-        });
-    }])
-
-    .controller('UserCtrl', ['$scope', '$http', function ($scope, $http) {
-        $http.get('/api/v.1/user/settings').success(function(data) {
-            $scope.user = data;
         });
     }]);
--- public/js/services.js
+++ public/js/services.js
@@ -4,29 +4,42 @@
 
 'use strict';
 
-angular.module('K12.services', []).factory('AuthSvc', function($http, $cookieStore) {
-    var currentUser = $cookieStore.get('_k12_user');
-    $cookieStore.remove('_k12_user');
+angular.module('K12.services', [])
+    .factory('AuthSvc', function($http, $cookieStore) {
+        var currentUser = $cookieStore.get('_k12_user');
+        $cookieStore.remove('_k12_user');
 
-    return {
-        signin: function(user, success, error) {
-            $http.post('/api/v.1/auth/signin', user).success(function(user){
-                currentUser = user;
-                success();
-            }).error(function(msg) {
-                error(msg);
-            });
-        },
+        return {
+            signin: function(user, success, error) {
+                $http.post('/api/v.1/auth/signin', user).success(function(user){
+                    currentUser = user;
+                    success();
+                }).error(function(msg) {
+                    error(msg);
+                });
+            },
 
-        signout: function(success, error) {
-            $http.get('/api/v.1/auth/signout').success(function(){
-                currentUser = {};
-                success();
-            }).error(error);
-        },
+            signout: function(success, error) {
+                $http.get('/api/v.1/auth/signout').success(function(){
+                    currentUser = {};
+                    success();
+                }).error(error);
+            },
 
-        user: function () {
-            return currentUser;
-        },
-    };
-});
+            user: function () {
+                return currentUser;
+            }
+        };
+    })
+
+    .factory('UserSvc', function($http) {
+        return {
+            settings: function(user, success, error) {
+                $http.get('/api/v.1/user/settings').success(function(settings) {
+                    success(settings);
+                }).error(function(msg) {
+                    error(msg);
+                });
+            }
+        };
+    });
